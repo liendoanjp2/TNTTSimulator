f900243801a3917977b9777a461e06ce2dce3cc4
Michael Tran <michaeltran20052006@yahoo.com>
2021-01-07 19:29:40 -0600

    Added new plugins iTween for movement tweening + finished basic movement + rolling

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rigidbody;
     public Animator animator;
     private Transform transform;
+    private STATE playerState = STATE.NORMAL;
 
     private const float leftDirection = -1f;
     private const float rightDirection = 1f;
@@ -21,10 +22,68 @@ public class PlayerMovement : MonoBehaviour
     private List<string> keyPresses = new List<string>();
 
     private Vector2 velocity = Vector2.zero;
-    private float timeSinceLastRolled = 0;
-    private const int rollCooldown = 0; // 3 seconds
+    private float timeSinceLastRolled = -2;
+    private const int rollCooldown = 2; // 3 seconds
 
-    private bool shouldRoll = false;
+    // Dodge variables
+    float rollDistance = 5f;
+
+    private Coroutine _dodging;
+
+
+    enum STATE
+    {
+        NORMAL, ROLLING
+    }
+
+
+    private IEnumerator DodgeCoroutine()
+    {
+        var endOfFrame = new WaitForEndOfFrame();
+        var rigidbody = GetComponent<Rigidbody2D>();
+        var transform = GetComponent<Transform>();
+        var boxCollider = GetComponent<BoxCollider2D>();
+        Vector2 characterBoxDim = boxCollider.size;
+
+        Vector2 distanceAbleToDodge = Vector2.one * rollDistance;
+
+        RaycastHit2D hitX = Physics2D.Raycast(transform.position, transform.right, rollDistance);
+        if (hitX.collider != null)
+        {
+            distanceAbleToDodge.x = hitX.distance - characterBoxDim.x / 2;
+        }
+
+        RaycastHit2D hitYUp = Physics2D.Raycast(transform.position, transform.up, rollDistance);
+        if (hitYUp.collider != null)
+        {
+            distanceAbleToDodge.y = hitYUp.distance - characterBoxDim.y / 2;
+        }
+
+        RaycastHit2D hitYDown = Physics2D.Raycast(transform.position, -transform.up, rollDistance);
+        if (hitYDown.collider != null)
+        {
+            distanceAbleToDodge.y = hitYDown.distance - characterBoxDim.y / 2;
+        }
+
+        Vector3 dodgePosition = rigidbody.position + movement.normalized * distanceAbleToDodge;
+
+        if (movement == Vector2.zero)
+        {
+            dodgePosition = transform.position + transform.right.normalized * distanceAbleToDodge.x;
+        }
+
+        print(distanceAbleToDodge + "" + dodgePosition);
+
+        playerState = STATE.ROLLING;
+
+        iTween.MoveTo(gameObject, iTween.Hash("position", dodgePosition, "time", 0.2f, "easeType", "easeInOutExpo", "oncomplete", "onDodgeComplete"));
+
+        // Increase character movespeed
+        currMoveSpeed = Mathf.Clamp(currMoveSpeed + rollingMoveSpeed, minMoveSpeed, maxMoveSpeed);
+        yield return endOfFrame;
+
+        _dodging = null;
+    }
 
     void Start()
     {
@@ -97,10 +156,10 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Direction", directionFacing);
         animator.SetBool("isRunning", currMoveSpeed > runningMoveSpeed);
 
-        if (shouldRoll)
+        if (playerState == STATE.ROLLING)
         {
             animator.Play("Player_Roll");
-            shouldRoll = false;
+            playerState = STATE.NORMAL;
         }
     }
 
@@ -122,7 +181,7 @@ public class PlayerMovement : MonoBehaviour
         Vector2 baseMovement = (rigidbody.position + movement * currMoveSpeed * Time.fixedDeltaTime);
 
         // Movement
-        rigidbody.MovePosition(baseMovement + velocity);
+        rigidbody.MovePosition(baseMovement);
         
         if (keyPresses.Count > 0)
         {
@@ -130,19 +189,8 @@ public class PlayerMovement : MonoBehaviour
             keyPresses.RemoveAt(0);
             if (keyPress.Equals("q") && Time.fixedTime - timeSinceLastRolled > rollCooldown)
             {
-                if (movement != Vector2.zero)
-                {
-                    // Take movement get the vector and add force
-                    velocity = new Vector2(movement.x, movement.y).normalized * rollingMoveSpeed;
-                }
-                else
-                {
-                    // Roll in direction you are facing
-                    velocity = transform.right * rollingMoveSpeed;
-                }
                 print("ROLLED");
-                currMoveSpeed = Mathf.Clamp(currMoveSpeed + rollingMoveSpeed, minMoveSpeed, maxMoveSpeed);
-                shouldRoll = true;
+                _dodging = StartCoroutine(DodgeCoroutine());
                 timeSinceLastRolled = Time.fixedTime;
             }
             else if (Input.GetKeyDown("w"))
