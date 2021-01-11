049a5bbb1f9f4cc54532e14fc0d889d2808275be
Philips Nguyen <philips.nguyen96@gmail.com>
2021-01-11 00:04:42 -0600

    Nice Reminder UI, not sure how to toggle on command though

---
+++ Assets/Scripts/ReminderDig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReminderDig : MonoBehaviour
+{
+    public GameObject fullReminderObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
